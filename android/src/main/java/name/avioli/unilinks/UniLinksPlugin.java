fe81388bb298b9d523c1c270e06e9fa289cc79be
alicja-ogonowska <kontakt@alicjaogonowska.pl>
2021-12-06 15:29:46 +0100

    Update imports.

--- android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
+++ android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
@@ -15,6 +15,7 @@ import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class UniLinksPlugin
         implements FlutterPlugin,
